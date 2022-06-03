e88e0e5225c20b57af1db023b45886249bd475e8
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-06-02 20:46:00 -0500

    Created connection to the DB when application starts

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const fs = require("fs");
+require('dotenv').config()
+
+const db = mysql.createConnection(
+    {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    },
+    console.log(`Connected to ${process.env.DB_NAME} database`)
+);
 
 async function displayMainMenu() {
     return await inquirer.prompt([
@@ -15,6 +26,7 @@ async function displayMainMenu() {
 
 async function init() {
     let keepExecuting = true;
+
     while (keepExecuting) {
         let option = await displayMainMenu();
         console.log(option);
